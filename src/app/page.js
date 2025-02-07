5c5931b62c03698d88df7d4b827cc79d0c5bf141
kashyapprajapat <prajapatikashyap14@gmail.com>
2025-02-07 20:15:44 +0530

    Feat:Add Seo keywords & setup with favicon

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,12 @@
 import YoutubePicker from "@/components/YoutubePicker";
 
+import { Metadata } from "next";
+
+export const metadata = {
+  title: "YouTube Comment Picker - Random Winner Selector",
+};
+
+
 export default function Home() {
   return (
     <div className="min-h-screen flex items-center justify-center p-8 sm:p-20">
